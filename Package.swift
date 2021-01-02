7daf5a4e6c83b669b981abd6bdeff74ebc0e0829
Paweł Hofman <pawel@codetitans.pl>
2021-01-02 22:41:35 +0100

    Updated reference to new v1.1 binary release

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GreatSDK",
-            url: "https://github.com/phofman/great-sdk/releases/download/v1.0.0/GreatSDK.xcframework.zip",
-            checksum: "e203ad45f4dbcb1fe38b6f3c7d658d8d8c8fb946415ad61bdff2aa1510d47a3e"),
+            url: "https://github.com/phofman/great-sdk/releases/download/v1.1.0/GreatSDK.xcframework.zip",
+            checksum: "784f2a64df2534714313d2808fc619832b0589fb51cde47b5a3effec448a4ac2"),
     ]
 )
